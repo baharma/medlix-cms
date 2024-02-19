565bd74a6499818e77f70d94a7a3ac8f0a944848
miartayasa <miartayasa10@gmail.com>
2024-02-19 17:58:07 +0800

    fix: dropify  inp

--- resources/views/components/componen-form/input-image-dropify.blade.php
+++ resources/views/components/componen-form/input-image-dropify.blade.php
@@ -1,18 +1,17 @@
 @props([
-
-    'wireModel'=>null,
-    'imageDefault'=>null,
-    'label'=>null,
-    'name'=>null,
-    'classInputValidate'
+    'wireModel' => null,
+    'imageDefault' => null,
+    'label' => null,
+    'name' => null,
+    'classInputValidate' => null,
     'id' => null,
-
 ])
 <label for="inputAddress4" class="col-sm-3 col-form-label">{{ $label }}</label>
 <div class="col" wire:ignore>
 
-    <input type="file" class="dropify {{$classInputValidate}}" id="{{ $id ?? $name }}" wire:model='{{ $wireModel }}'
-        name="{{ $name }}" data-default-file="{{ $imageDefault }}" data-max-file-size="2M">
+    <input type="file" class="dropify {{ $classInputValidate }}" id="{{ $id ?? $name }}"
+        wire:model='{{ $wireModel }}' name="{{ $name }}" data-default-file="{{ $imageDefault }}"
+        data-max-file-size="2M">
 </div>
 @error('{{ $name }}')
     <span class="error">{{ $message }}</span>
