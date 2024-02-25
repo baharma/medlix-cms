1073b376dba749a38519794dada4a4f12dd80f2e
baharma <bagusaditya1899@gmail.com>
2024-02-26 01:37:29 +0800

    produk done

--- app/Livewire/Pages/Produk/Produk.php
+++ app/Livewire/Pages/Produk/Produk.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\Pages\Produk;
 
+use App\Models\Product;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Produk extends Component
 {
+    #[Title('Produk')]
+    public $model;
+
+    public function mount(Product $product){
+        $this->model = $product;
+    }
+    public function confirmDelete($get_id){
+        $this->model->find($get_id)->delete();
+    }
+
     public function render()
     {
-        return view('livewire.pages.produk.produk');
+        $data = $this->model->where('app_id',Auth::user()->default_cms)->get();
+        return view('livewire.pages.produk.produk',compact('data'));
     }
+
 }
--- resources/views/livewire/pages/produk/produk.blade.php
+++ resources/views/livewire/pages/produk/produk.blade.php
@@ -1,3 +1,41 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <div class="d-flex flex-row-reverse bd-highlight mb-4">
+        <a type="button" class="btn btn-primary" href="{{route('produck-form')}}">
+            <i class='bx bxs-add-to-queue'></i>Add New Produk</a>
+    </div>
+    <div class="row">
+        @foreach ($data as $items )
+        <div class="col">
+            <div class="card" style="width: 400px;">
+                <img src="{{$items->image}}"
+                    class="card-img-top" alt="...">
+                <div class="card-body">
+                    <img src="{{$items->logo}}"
+                        class="card-img-top" alt="..." style="height: 100px;object-fit:cover">
+                    <p class="card-text">{{$items->text}}</p>
+                    <div class="d-flex justify-content-between">
+                        <div class="mx-2">
+                            <a href="{{route('produck-form',$items->id)}}" class="btn btn-warning">
+                                <i class='bx bxs-edit-alt'></i>
+                                Edit</a>
+                        </div>
+                        <div class="mx-2">
+                            <a href="#" class="btn btn-danger"  @click="$dispatch('confirm-delete', { get_id: {{ $items->id }} })">
+                                <i class='bx bxs-trash-alt'></i>
+                                Delete</a>
+                        </div>
+                        <div class="mx-2">
+                            <a href="{{$items->url}}" class="btn btn-info">
+                                <i class='bx bx-link-alt'></i>
+                                Link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+
+        @include('layouts.component.confirm-delete')
+    </div>
+
 </div>
