7d39f2e4acbd6c33bca4fae3b30f0ba10da4063c
baharma <bagusaditya1899@gmail.com>
2024-02-25 23:40:07 +0800

    ad

---
+++ app/Livewire/Pages/Produk/Produk.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\Produk;
+
+use Livewire\Component;
+
+class Produk extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.produk.produk');
+    }
+}
---
+++ app/Livewire/Pages/Produk/ProdukFormModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\Produk;
+
+use Livewire\Component;
+
+class ProdukFormModel extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.produk.produk-form-model');
+    }
+}
