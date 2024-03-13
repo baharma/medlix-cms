513f2796cd18063324fc2e01c1dd6aad742d587b
I Gede Miarta Yasa <miartayasa10@gmail.com>
2024-03-13 15:43:44 +0800

    fix: migration and assets file

--- database/seeders/AppSectionSeeder.php
+++ database/seeders/AppSectionSeeder.php
@@ -13,13 +13,38 @@ class AppSectionSeeder extends Seeder
      */
     public function run(): void
     {
-        AppSection::create(['app_id'=>2,'section_id'=>1]);
-        AppSection::create(['app_id'=>2,'section_id'=>2]);
-        AppSection::create(['app_id'=>2,'section_id'=>4]);
-        AppSection::create(['app_id'=>2,'section_id'=>5]);
-        AppSection::create(['app_id'=>2,'section_id'=>6]);
-        AppSection::create(['app_id'=>2,'section_id'=>7]);
-        AppSection::create(['app_id'=>2,'section_id'=>8]);
-        AppSection::create(['app_id'=>2,'section_id'=>9]);
+        $data = [
+            ['app_id' => 1, 'section_id' => 1],
+            ['app_id' => 1, 'section_id' => 5],
+            ['app_id' => 1, 'section_id' => 6],
+            ['app_id' => 1, 'section_id' => 8],
+            ['app_id' => 1, 'section_id' => 11],
+            ['app_id' => 1, 'section_id' => 14],
+            ['app_id' => 1, 'section_id' => 16],
+            ['app_id' => 1, 'section_id' => 18],
+            ['app_id' => 1, 'section_id' => 22],
+            ['app_id' => 1, 'section_id' => 23],
+            ['app_id' => 2, 'section_id' => 1],
+            ['app_id' => 2, 'section_id' => 2],
+            ['app_id' => 2, 'section_id' => 3],
+            ['app_id' => 2, 'section_id' => 7],
+            ['app_id' => 2, 'section_id' => 8],
+            ['app_id' => 2, 'section_id' => 9],
+            ['app_id' => 2, 'section_id' => 17],
+            ['app_id' => 2, 'section_id' => 19],
+            ['app_id' => 2, 'section_id' => 21],
+            ['app_id' => 3, 'section_id' => 1],
+            ['app_id' => 3, 'section_id' => 4],
+            ['app_id' => 3, 'section_id' => 8],
+            ['app_id' => 3, 'section_id' => 10],
+            ['app_id' => 3, 'section_id' => 12],
+            ['app_id' => 3, 'section_id' => 13],
+            ['app_id' => 3, 'section_id' => 15],
+            ['app_id' => 3, 'section_id' => 20],
+        ];
+
+        foreach ($data as $record) {
+            AppSection::create($record);
+        }
     }
 }
