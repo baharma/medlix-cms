e0efab345393f2173144daa3c55515de5966b588
I Gede Miarta Yasa <miartayasa10@gmail.com>
2024-04-02 20:35:23 +0800

    remove navigate

--- resources/views/livewire/components/sidebar.blade.php
+++ resources/views/livewire/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div>
     @foreach ($section as $side)
         <li>
-            <a href="{{ url($side->section->url) }}" wire:navigate>
+            <a href="{{ url($side->section->url) }}">
                 <div class="parent-icon"><i class="{{ $side->section->icon }}"></i>
                 </div>
                 <div class="menu-title">{{ $side->section->name }}</div>
