968f3c041ed8f64df1368a7deda6936586b77dd5
I Gede Miarta Yasa <miartayasa10@gmail.com>
2024-04-03 23:01:35 +0800

    update image position

--- resources/views/livewire/pages/porto.blade.php
+++ resources/views/livewire/pages/porto.blade.php
@@ -275,7 +275,8 @@
                     <hr>
                     <div class="row">
                         @foreach ($award as $item)
-                            <div class="card col-md-4 mr-3 ml-3" style="width: 18rem;">
+                            <div class="card col-md-4 mr-3 ml-3"
+                                style="width: 18rem; margin-left: 10px; margin-right: 10px;">
                                 <div class="card-body text-center">
                                     <img src="{{ asset($item->title) }}" alt="" style="max-width: 80px">
                                     <br>
